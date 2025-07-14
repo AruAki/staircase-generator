8c4a08990806c26854eccca1d6f6a197db69d626
Danielle Hiebert <dhiebert2013@gmail.com>
2025-07-13 23:10:55 -0600

    chore: remove unused commented code

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -112,7 +112,6 @@ export function drawCircle(
 		if (i === 0 && numberOfSegments % 2 === 1) {
 			color = '#0000ff';
 		}
-		// console.log(hue, color);
 		drawWedge(pixels, cx, cy, radius, startAngle, endAngle, color);
 	}
 }
@@ -164,66 +163,4 @@ export function drawWedge(
 }
 export function genRandomColor() {
 	return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-function HSVtoRGB(h: number, s: number, v: number) {
-	var r, g, b, i, f, p, q, t;
-	i = Math.floor(h * 6);
-	f = h * 6 - i;
-	p = v * (1 - s);
-	q = v * (1 - f * s);
-	t = v * (1 - (1 - f) * s);
-	switch (i % 6) {
-		case 0:
-			((r = v), (g = t), (b = p));
-			break;
-		case 1:
-			((r = q), (g = v), (b = p));
-			break;
-		case 2:
-			((r = p), (g = v), (b = t));
-			break;
-		case 3:
-			((r = p), (g = q), (b = v));
-			break;
-		case 4:
-			((r = t), (g = p), (b = v));
-			break;
-		case 5:
-			((r = v), (g = p), (b = q));
-			break;
-	}
-	return {
-		r: Math.round(r! * 255),
-		g: Math.round(g! * 255),
-		b: Math.round(b! * 255),
-	};
-}
-function RGBtoHEX(r: number, g: number, b: number) {
-	return `#${NumberToHex(r)}${NumberToHex(g)}${NumberToHex(b)}`;
-}
-function NumberToHex(n: number) {
-	return n.toString(16).padStart(2, '0');
-}
-
-function gcd(a: number, b: number) {
-	while (b !== 0) {
-		const temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return a;
-}
-
-function isCoprime(a: number, b: number) {
-	return gcd(a, b) === 1;
-}
-
-function lowestCoprime(n: number) {
-	for (let i = 2; i < n; i++) {
-		if (isCoprime(i, n)) {
-			return i; // Return the first (lowest) coprime > 1
-		}
-	}
-	return null; // No coprime found (shouldn't happen for n > 2)
 }
