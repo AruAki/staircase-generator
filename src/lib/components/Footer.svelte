715268572b1e5af0dcd50509c2837f9c7cb78a64
Danielle Hiebert <dhiebert2013@gmail.com>
2025-07-16 12:28:09 -0600

    feat: footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Github } from '@o7/icon';
+</script>
+
+<div class="flex flex-row items-center justify-center p-4">
+	<div
+		class="flex w-full max-w-2xl flex-row justify-between border-t border-zinc-700 px-4 pt-4"
+	>
+		<span>&copy; Aru Aki {new Date().getFullYear()}</span>
+		<a
+			href="https://github.com/AruAki/staircase-generator"
+			class="flex flex-row items-center gap-2 hover:text-purple-500"
+		>
+			<Github />
+			<span>View on GitHub</span>
+		</a>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,6 @@
 	import Select from '$lib/components/Select.svelte';
 	import { drawCircle } from '$lib/functions';
 	import type { SegmentColoringMethod } from '$lib/types';
-	import { Github } from '@o7/icon';
 	import { onMount } from 'svelte';
 
 	let pixels: string[][] = $state([]);
@@ -74,17 +73,8 @@
 	<title>Aru Aki's Pixel Spiral & Curved Staircase Generator</title>
 </svelte:head>
 
-<div
-	class="flex min-h-screen w-full flex-col items-center justify-center gap-6 p-4"
->
+<div class="flex min-h-screen w-full flex-col items-center gap-6 p-4">
 	<h1 class="text-4xl">Aru Aki's Pixel Spiral & Curved Staircase Generator</h1>
-	<a
-		href="https://github.com/AruAki/staircase-generator"
-		class="fixed bottom-1.5 right-3 flex flex-row items-center gap-2 rounded-lg bg-zinc-700 px-3 py-2"
-	>
-		<Github />
-		<span>View on GitHub</span>
-	</a>
 	<div class="flex flex-col gap-4">
 		<div class="flex flex-row gap-2">
 			<Input
